90b43a6aa512a10309592e175893720c6a41a9b2
pozer <pozer@c95d2ef9-caf5-0310-aa31-e80852a18daa>
2005-05-03 13:34:41 +0000

    Force lists in bicolor mode - ticket:4

--- src/gmysqlcc_gui_server.c
+++ src/gmysqlcc_gui_server.c
@@ -76,46 +76,46 @@ gboolean gmysqlcc_gui_server_delete (p_gmysqlcc_gui_server gui_server) {
 }
 
 void gmysqlcc_gui_server_create_widget (p_gmysqlcc_gui_server gui_server) {
-  GtkWidget *statusbar1;
-  GtkWidget *vbox1, *vbox2, *vbox3;
-  GtkWidget *hpaned1;
-  GtkWidget *hbox2, *hbox3;
-  GtkWidget *label1, *label2, *label3, *label6, *label7;
-  GtkWidget *notebook1;
-  GtkWidget *scrolledwindow1, *scrolledwindow2;
-  GtkWidget *toolbar1;
+	GtkWidget *statusbar1;
+	GtkWidget *vbox1, *vbox2, *vbox3;
+	GtkWidget *hpaned1;
+	GtkWidget *hbox2, *hbox3;
+	GtkWidget *label1, *label2, *label3, *label6, *label7;
+	GtkWidget *notebook1;
+	GtkWidget *scrolledwindow1, *scrolledwindow2;
+	GtkWidget *toolbar1;
 	GtkWidget * imgToolbar;
-  GtkToolItem * btnTlbrClose, * btnTlbrSql, * btnTlbrSqlFile;
+	GtkToolItem * btnTlbrClose, * btnTlbrSql, * btnTlbrSqlFile;
 	GtkWidget *btnDbAdd, *btnDbDel;
-  GtkWidget *btnTblAdd, *btnTblEdit, *btnTblDump, *btnTblDel;
+	GtkWidget *btnTblAdd, *btnTblEdit, *btnTblDump, *btnTblDel;
 	GtkWidget *mnuDBOpsRefresh;
 	GtkWidget *mnuDBOpsShowCreate;
 	GtkWidget *mnuTBLOpsShowCreate;
 	GtkTreeSelection *select;
 	GtkTooltips * tooltips;
 	GString * sTitle;
-	
+
 	tooltips = gtk_tooltips_new();
-	
-  gui_server->window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+
+	gui_server->window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 	sTitle = g_string_new("");
 	g_string_printf(sTitle, _("Server : %s"), gui_server->mysql_srv->name);
-  gtk_window_set_title (GTK_WINDOW (gui_server->window), sTitle->str);
+	gtk_window_set_title (GTK_WINDOW (gui_server->window), sTitle->str);
 	g_string_free(sTitle, TRUE);
 	gtk_window_set_default_size (GTK_WINDOW (gui_server->window), 400, 300);
 	g_signal_connect (G_OBJECT (gui_server->window), "destroy", 
 										G_CALLBACK (gmysqlcc_gui_server_evt_destroy), (gpointer)gui_server);
 
-  vbox1 = gtk_vbox_new (FALSE, 0);
-  gtk_widget_show (vbox1);
-  gtk_container_add (GTK_CONTAINER (gui_server->window), vbox1);
-	
-	
-	
-  toolbar1 = gtk_toolbar_new ();
-  gtk_widget_show (toolbar1);
-  gtk_box_pack_start (GTK_BOX (vbox1), toolbar1, FALSE, FALSE, 0);
-  gtk_toolbar_set_style (GTK_TOOLBAR (toolbar1), GTK_TOOLBAR_BOTH_HORIZ);
+	vbox1 = gtk_vbox_new (FALSE, 0);
+	gtk_widget_show (vbox1);
+	gtk_container_add (GTK_CONTAINER (gui_server->window), vbox1);
+
+
+
+	toolbar1 = gtk_toolbar_new ();
+	gtk_widget_show (toolbar1);
+	gtk_box_pack_start (GTK_BOX (vbox1), toolbar1, FALSE, FALSE, 0);
+	gtk_toolbar_set_style (GTK_TOOLBAR (toolbar1), GTK_TOOLBAR_BOTH_HORIZ);
 
 	imgToolbar = gtk_image_new_from_stock(GTK_STOCK_EXECUTE, GTK_ICON_SIZE_LARGE_TOOLBAR);
 	gtk_widget_show(imgToolbar);
@@ -126,7 +126,7 @@ void gmysqlcc_gui_server_create_widget (p_gmysqlcc_gui_server gui_server) {
 	gtk_widget_show(GTK_WIDGET(btnTlbrSql));
 	gtk_toolbar_insert(GTK_TOOLBAR(toolbar1), GTK_TOOL_ITEM(btnTlbrSql), -1);
 	gtk_tool_item_set_tooltip (GTK_TOOL_ITEM(btnTlbrSql), tooltips, _("Exec SQL Query"), NULL);
-	
+
 	imgToolbar = gtk_image_new_from_stock(GTK_STOCK_EXECUTE, GTK_ICON_SIZE_LARGE_TOOLBAR);
 	gtk_widget_show(imgToolbar);
 	btnTlbrSqlFile = gtk_tool_button_new (imgToolbar, _("SQL File"));
@@ -136,7 +136,7 @@ void gmysqlcc_gui_server_create_widget (p_gmysqlcc_gui_server gui_server) {
 	gtk_widget_show(GTK_WIDGET(btnTlbrSqlFile));
 	gtk_toolbar_insert(GTK_TOOLBAR(toolbar1), GTK_TOOL_ITEM(btnTlbrSqlFile), -1);
 	gtk_tool_item_set_tooltip (GTK_TOOL_ITEM(btnTlbrSqlFile), tooltips, _("Exec SQL File"), NULL);
-	
+
 	imgToolbar = gtk_image_new_from_stock(GTK_STOCK_CLOSE, GTK_ICON_SIZE_LARGE_TOOLBAR);
 	gtk_widget_show(imgToolbar);
 	btnTlbrClose = gtk_tool_button_new (imgToolbar, _("Close"));
@@ -146,32 +146,33 @@ void gmysqlcc_gui_server_create_widget (p_gmysqlcc_gui_server gui_server) {
 	gtk_widget_show(GTK_WIDGET(btnTlbrClose));
 	gtk_toolbar_insert(GTK_TOOLBAR(toolbar1), GTK_TOOL_ITEM(btnTlbrClose), -1);
 	gtk_tool_item_set_tooltip (GTK_TOOL_ITEM(btnTlbrClose), tooltips, _("Close window"), NULL);
-	
-	
-	
-  notebook1 = gtk_notebook_new ();
-  gtk_widget_show (notebook1);
-  gtk_box_pack_start (GTK_BOX (vbox1), notebook1, TRUE, TRUE, 0);
 
-  hpaned1 = gtk_hpaned_new ();
-  gtk_widget_show (hpaned1);
-  gtk_container_add (GTK_CONTAINER (notebook1), hpaned1);
-  gtk_paned_set_position (GTK_PANED (hpaned1), 200);
-	
-	
-	
-  vbox2 = gtk_vbox_new (FALSE, 0);
-  gtk_widget_show (vbox2);
-  gtk_paned_pack1 (GTK_PANED (hpaned1), vbox2, FALSE, TRUE);
 
-  scrolledwindow1 = gtk_scrolled_window_new (NULL, NULL);
-  gtk_widget_show (scrolledwindow1);
-  gtk_box_pack_start (GTK_BOX (vbox2), scrolledwindow1, TRUE, TRUE, 0);
-  gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolledwindow1), GTK_POLICY_NEVER, GTK_POLICY_ALWAYS);
 
-  gui_server->lstBase = gtk_tree_view_new ();
-  gtk_widget_show (gui_server->lstBase);
-  gtk_container_add (GTK_CONTAINER (scrolledwindow1), gui_server->lstBase);
+	notebook1 = gtk_notebook_new ();
+	gtk_widget_show (notebook1);
+	gtk_box_pack_start (GTK_BOX (vbox1), notebook1, TRUE, TRUE, 0);
+
+	hpaned1 = gtk_hpaned_new ();
+	gtk_widget_show (hpaned1);
+	gtk_container_add (GTK_CONTAINER (notebook1), hpaned1);
+	gtk_paned_set_position (GTK_PANED (hpaned1), 200);
+
+
+
+	vbox2 = gtk_vbox_new (FALSE, 0);
+	gtk_widget_show (vbox2);
+	gtk_paned_pack1 (GTK_PANED (hpaned1), vbox2, FALSE, TRUE);
+
+	scrolledwindow1 = gtk_scrolled_window_new (NULL, NULL);
+	gtk_widget_show (scrolledwindow1);
+	gtk_box_pack_start (GTK_BOX (vbox2), scrolledwindow1, TRUE, TRUE, 0);
+	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolledwindow1), GTK_POLICY_NEVER, GTK_POLICY_ALWAYS);
+
+	gui_server->lstBase = gtk_tree_view_new ();
+	gtk_widget_show (gui_server->lstBase);
+	gtk_tree_view_set_rules_hint(GTK_TREE_VIEW (gui_server->lstBase), TRUE);
+	gtk_container_add (GTK_CONTAINER (scrolledwindow1), gui_server->lstBase);
 	g_signal_connect (G_OBJECT (gui_server->lstBase), "button-press-event", 
 										G_CALLBACK (gmysqlcc_gui_server_evt_lstBase_btnpress), (gpointer)gui_server);
 	select = gtk_tree_view_get_selection (GTK_TREE_VIEW (gui_server->lstBase));
@@ -179,36 +180,36 @@ void gmysqlcc_gui_server_create_widget (p_gmysqlcc_gui_server gui_server) {
 	g_signal_connect (G_OBJECT (select), "changed", 
 										G_CALLBACK (gmysqlcc_gui_server_evt_lstBase_selected), (gpointer)gui_server);
 
-  hbox2 = gtk_hbox_new (FALSE, 0);
-  gtk_widget_show (hbox2);
-  gtk_box_pack_start (GTK_BOX (vbox2), hbox2, FALSE, TRUE, 0);
+	hbox2 = gtk_hbox_new (FALSE, 0);
+	gtk_widget_show (hbox2);
+	gtk_box_pack_start (GTK_BOX (vbox2), hbox2, FALSE, TRUE, 0);
 
- 	btnDbAdd = createIconButton("gtk-add", _("Add"));
-  gtk_widget_show (btnDbAdd);
-  gtk_box_pack_start (GTK_BOX (hbox2), btnDbAdd, TRUE, TRUE, 0);
+	btnDbAdd = createIconButton("gtk-add", _("Add"));
+	gtk_widget_show (btnDbAdd);
+	gtk_box_pack_start (GTK_BOX (hbox2), btnDbAdd, TRUE, TRUE, 0);
 	g_signal_connect (G_OBJECT (btnDbAdd), "clicked", 
 										G_CALLBACK (gmysqlcc_gui_server_evt_btnDbAdd_clicked), (gpointer)gui_server);
 
- 	btnDbDel = createIconButton("gtk-delete", _("Delete"));
+	btnDbDel = createIconButton("gtk-delete", _("Delete"));
 	gtk_widget_show (btnDbDel);
-  gtk_box_pack_start (GTK_BOX (hbox2), btnDbDel, TRUE, TRUE, 0);
+	gtk_box_pack_start (GTK_BOX (hbox2), btnDbDel, TRUE, TRUE, 0);
 	g_signal_connect (G_OBJECT (btnDbDel), "clicked", 
 										G_CALLBACK (gmysqlcc_gui_server_evt_btnDbDel_clicked), (gpointer)gui_server);
-	
-	
-	
-  vbox3 = gtk_vbox_new (FALSE, 0);
-  gtk_widget_show (vbox3);
-  gtk_paned_pack2 (GTK_PANED (hpaned1), vbox3, TRUE, TRUE);
 
-  scrolledwindow2 = gtk_scrolled_window_new (NULL, NULL);
-  gtk_widget_show (scrolledwindow2);
-  gtk_box_pack_start (GTK_BOX (vbox3), scrolledwindow2, TRUE, TRUE, 0);
-  gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolledwindow2), GTK_POLICY_NEVER, GTK_POLICY_ALWAYS);
 
-  gui_server->lstTable = gtk_tree_view_new ();
-  gtk_widget_show (gui_server->lstTable);
-  gtk_container_add (GTK_CONTAINER (scrolledwindow2), gui_server->lstTable);
+	vbox3 = gtk_vbox_new (FALSE, 0);
+	gtk_widget_show (vbox3);
+	gtk_paned_pack2 (GTK_PANED (hpaned1), vbox3, TRUE, TRUE);
+
+	scrolledwindow2 = gtk_scrolled_window_new (NULL, NULL);
+	gtk_widget_show (scrolledwindow2);
+	gtk_box_pack_start (GTK_BOX (vbox3), scrolledwindow2, TRUE, TRUE, 0);
+	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolledwindow2), GTK_POLICY_NEVER, GTK_POLICY_ALWAYS);
+
+	gui_server->lstTable = gtk_tree_view_new ();
+	gtk_widget_show (gui_server->lstTable);
+	gtk_container_add (GTK_CONTAINER (scrolledwindow2), gui_server->lstTable);
+	gtk_tree_view_set_rules_hint(GTK_TREE_VIEW (gui_server->lstTable), TRUE);
 	g_signal_connect (G_OBJECT (gui_server->lstTable), "button-press-event", 
 										G_CALLBACK (gmysqlcc_gui_server_evt_lstTable_btnpress), (gpointer)gui_server);
 	select = gtk_tree_view_get_selection (GTK_TREE_VIEW (gui_server->lstTable));
@@ -216,71 +217,71 @@ void gmysqlcc_gui_server_create_widget (p_gmysqlcc_gui_server gui_server) {
 	g_signal_connect (G_OBJECT (select), "changed", 
 										G_CALLBACK (gmysqlcc_gui_server_evt_lstTable_selected), (gpointer)gui_server);
 
-  hbox3 = gtk_hbox_new (FALSE, 0);
-  gtk_widget_show (hbox3);
-  gtk_box_pack_start (GTK_BOX (vbox3), hbox3, FALSE, TRUE, 0);
+	hbox3 = gtk_hbox_new (FALSE, 0);
+	gtk_widget_show (hbox3);
+	gtk_box_pack_start (GTK_BOX (vbox3), hbox3, FALSE, TRUE, 0);
 
 	btnTblAdd = createIconButton("gtk-add", _("Add"));
 	gtk_widget_show (btnTblAdd);
-  gtk_box_pack_start (GTK_BOX (hbox3), btnTblAdd, TRUE, TRUE, 0);
+	gtk_box_pack_start (GTK_BOX (hbox3), btnTblAdd, TRUE, TRUE, 0);
 	g_signal_connect (G_OBJECT (btnTblAdd), "clicked", 
 										G_CALLBACK (gmysqlcc_gui_server_evt_btnTblAdd_clicked), (gpointer)gui_server);
 
 	btnTblEdit = createIconButton("gtk-apply", _("Modify"));
-  gtk_widget_show (btnTblEdit);
-  gtk_box_pack_start (GTK_BOX (hbox3), btnTblEdit, TRUE, TRUE, 0);
+	gtk_widget_show (btnTblEdit);
+	gtk_box_pack_start (GTK_BOX (hbox3), btnTblEdit, TRUE, TRUE, 0);
 	g_signal_connect (G_OBJECT (btnTblEdit), "clicked", 
 										G_CALLBACK (gmysqlcc_gui_server_evt_btnTblEdit_clicked), (gpointer)gui_server);
 
 	btnTblDump = createIconButton("gtk-save", _("Dump"));
-  gtk_widget_show (btnTblDump);
-  gtk_box_pack_start (GTK_BOX (hbox3), btnTblDump, TRUE, TRUE, 0);
+	gtk_widget_show (btnTblDump);
+	gtk_box_pack_start (GTK_BOX (hbox3), btnTblDump, TRUE, TRUE, 0);
 	g_signal_connect (G_OBJECT (btnTblDump), "clicked", 
 										G_CALLBACK (gmysqlcc_gui_server_evt_btnTblDump_clicked), (gpointer)gui_server);
 
 	btnTblDel = createIconButton("gtk-delete", _("Delete"));
-  gtk_widget_show (btnTblDel);
-  gtk_box_pack_start (GTK_BOX (hbox3), btnTblDel, TRUE, TRUE, 0);
+	gtk_widget_show (btnTblDel);
+	gtk_box_pack_start (GTK_BOX (hbox3), btnTblDel, TRUE, TRUE, 0);
 	g_signal_connect (G_OBJECT (btnTblDel), "clicked", 
 										G_CALLBACK (gmysqlcc_gui_server_evt_btnTblDel_clicked), (gpointer)gui_server);
 
-  label1 = gtk_label_new (_("Databases"));
-  gtk_widget_show (label1);
-  gtk_notebook_set_tab_label (GTK_NOTEBOOK (notebook1), gtk_notebook_get_nth_page (GTK_NOTEBOOK (notebook1), 0), label1);
-  gtk_label_set_justify (GTK_LABEL (label1), GTK_JUSTIFY_LEFT);
-	
-	
-	
-  label6 = gtk_label_new (_("In construction"));
-  gtk_widget_show (label6);
-  gtk_container_add (GTK_CONTAINER (notebook1), label6);
-  gtk_label_set_justify (GTK_LABEL (label6), GTK_JUSTIFY_LEFT);
+	label1 = gtk_label_new (_("Databases"));
+	gtk_widget_show (label1);
+	gtk_notebook_set_tab_label (GTK_NOTEBOOK (notebook1), gtk_notebook_get_nth_page (GTK_NOTEBOOK (notebook1), 0), label1);
+	gtk_label_set_justify (GTK_LABEL (label1), GTK_JUSTIFY_LEFT);
 
-  label2 = gtk_label_new (_("Users"));
-  gtk_widget_show (label2);
-  gtk_notebook_set_tab_label (GTK_NOTEBOOK (notebook1), gtk_notebook_get_nth_page (GTK_NOTEBOOK (notebook1), 1), label2);
-  gtk_label_set_justify (GTK_LABEL (label2), GTK_JUSTIFY_LEFT);
-	
-	
-	
-  label7 = gtk_label_new (_("In construction"));
-  gtk_widget_show (label7);
-  gtk_container_add (GTK_CONTAINER (notebook1), label7);
-  gtk_label_set_justify (GTK_LABEL (label7), GTK_JUSTIFY_LEFT);
 
-  label3 = gtk_label_new (_("Server"));
-  gtk_widget_show (label3);
-  gtk_notebook_set_tab_label (GTK_NOTEBOOK (notebook1), gtk_notebook_get_nth_page (GTK_NOTEBOOK (notebook1), 2), label3);
-  gtk_label_set_justify (GTK_LABEL (label3), GTK_JUSTIFY_LEFT);
-	
-	
-	
-  statusbar1 = gtk_statusbar_new ();
-  gtk_widget_show (statusbar1);
-  gtk_box_pack_start (GTK_BOX (vbox1), statusbar1, FALSE, FALSE, 0);
-	
-	
-	
+
+	label6 = gtk_label_new (_("In construction"));
+	gtk_widget_show (label6);
+	gtk_container_add (GTK_CONTAINER (notebook1), label6);
+	gtk_label_set_justify (GTK_LABEL (label6), GTK_JUSTIFY_LEFT);
+
+	label2 = gtk_label_new (_("Users"));
+	gtk_widget_show (label2);
+	gtk_notebook_set_tab_label (GTK_NOTEBOOK (notebook1), gtk_notebook_get_nth_page (GTK_NOTEBOOK (notebook1), 1), label2);
+	gtk_label_set_justify (GTK_LABEL (label2), GTK_JUSTIFY_LEFT);
+
+
+
+	label7 = gtk_label_new (_("In construction"));
+	gtk_widget_show (label7);
+	gtk_container_add (GTK_CONTAINER (notebook1), label7);
+	gtk_label_set_justify (GTK_LABEL (label7), GTK_JUSTIFY_LEFT);
+
+	label3 = gtk_label_new (_("Server"));
+	gtk_widget_show (label3);
+	gtk_notebook_set_tab_label (GTK_NOTEBOOK (notebook1), gtk_notebook_get_nth_page (GTK_NOTEBOOK (notebook1), 2), label3);
+	gtk_label_set_justify (GTK_LABEL (label3), GTK_JUSTIFY_LEFT);
+
+
+
+	statusbar1 = gtk_statusbar_new ();
+	gtk_widget_show (statusbar1);
+	gtk_box_pack_start (GTK_BOX (vbox1), statusbar1, FALSE, FALSE, 0);
+
+
+
 	gui_server->mnuBdOps = gtk_menu_new();
 	gtk_widget_show (gui_server->mnuBdOps);
 
@@ -290,14 +291,14 @@ void gmysqlcc_gui_server_create_widget (p_gmysqlcc_gui_server gui_server) {
 	g_signal_connect (G_OBJECT (mnuDBOpsRefresh), "activate", 
 										G_CALLBACK (gmysqlcc_gui_server_evt_mnuDBOpsRefresh_activate), 
 										(gpointer)gui_server);
-	
+
 	mnuDBOpsShowCreate = gtk_menu_item_new_with_label(_("Display all create structures"));
 	gtk_widget_show (mnuDBOpsShowCreate);
 	gtk_menu_append (GTK_MENU_SHELL(gui_server->mnuBdOps), mnuDBOpsShowCreate);
 	g_signal_connect (G_OBJECT (mnuDBOpsShowCreate), "activate", 
 										G_CALLBACK (gmysqlcc_gui_server_evt_mnuDBOpsShowCreate_activate), 
 										(gpointer)gui_server);
-	
+
 	gui_server->mnuTblOps = gtk_menu_new();
 	gtk_widget_show (gui_server->mnuTblOps);
 
